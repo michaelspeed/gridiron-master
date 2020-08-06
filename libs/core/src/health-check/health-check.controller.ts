8dcf66a6c4a713edb7ffbab47aa6aff5ee6bfa63
Risha Sringa Changmai <michael.the.don@gmail.com>
2020-08-06 16:14:52 +0530

    added health

--- libs/core/src/health-check/health-check.controller.ts
+++ libs/core/src/health-check/health-check.controller.ts
@@ -1,6 +1,13 @@
 import {Controller, Get} from '@nestjs/common';
 import {HEALTH_CHECK_ROUTE} from './constants';
-import {DNSHealthIndicator, HealthCheck, HealthCheckService} from '@nestjs/terminus';
+import {
+    DiskHealthIndicator,
+    DNSHealthIndicator,
+    HealthCheck,
+    HealthCheckService,
+    MemoryHealthIndicator,
+    TypeOrmHealthIndicator
+} from '@nestjs/terminus';
 import {HealthCheckRegistryService} from './health-check-registry.service';
 
 @Controller(HEALTH_CHECK_ROUTE)
@@ -8,12 +15,21 @@ export class HealthCheckController {
     constructor(
         private health: HealthCheckService,
         private dns: DNSHealthIndicator,
-        private healthCheckRegistryService: HealthCheckRegistryService
+        private healthCheckRegistryService: HealthCheckRegistryService,
+        private db: TypeOrmHealthIndicator,
+        private memory: MemoryHealthIndicator,
+        private disk: DiskHealthIndicator
     ) {}
 
     @Get()
     @HealthCheck()
     check() {
-        return this.health.check(this.healthCheckRegistryService.healthIndicatorFunctions)
+        return this.health.check([
+            async () => this.dns.pingCheck('google', 'https://www.google.com/'),
+            async () => this.db.pingCheck('database', {timeout: 300}),
+            async () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+            async () => this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
+            async () => this.disk.checkStorage('storage', { threshold: 250 * 1024 * 1024 * 1024, path: '/' }),
+        ])
     }
 }
